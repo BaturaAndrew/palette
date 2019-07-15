97120f4edd0e564b2220ba3d1e0ae4f4239b57d3
Batura Andrew <baturaandrew@gmainl.com>
2019-07-15 15:39:59 +0300

    feat: realise color action

--- palette/animation.js
+++ palette/animation.js
@@ -1,43 +1,163 @@
-const node = document.querySelector('#node');
+// icons for cursors
+const bucketCursor = "url(asserts/images/fill-drip-solid.png) 10 10, auto";
+const chooseColorCursor = "url(asserts/images/eye-dropper-solid.png) 10 10, auto";
+const moveCursor = "url(asserts/images/expand-arrows-alt-solid.png) 10 10, auto";
+const transferCursor = "url(asserts/images/exchange-alt-solid.png) 10 10, auto";
 
-let currentX = 0;
-const endX = 300;
+// blocks
+const canvas = document.querySelector(".canvas");
+const pallete = document.querySelector(".pallete");
+const colors = document.querySelector(".colors");
 
-// function animate(endX, duration) {
+// tools
+const paintBucket = document.querySelector("#paintBucket");
+const chooseColor = document.querySelector("#chooseColor");
+const move = document.querySelector("#move");
+const transform = document.querySelector("#transform");
 
-//   let currentX = node.offsetLeft;
-//   const frameCount = duration / 1000 * 60;
-//   const dx = (endX - currentX) / frameCount;
+//  colors
+const currColor = document.querySelector("#curr-color");
+const prevColor = document.querySelector("#prev-color");
+const redColor = document.querySelector("#red-color");
+const blueColor = document.querySelector("#blue-color");
 
-//   const intervalId = setInterval(() => {
-//     currentX += dx;
-//     console.log(currentX);
-//     if (currentX > endX) {
-//       clearInterval(intervalId);
-//     }
-//     node.style.transform = 'translateX(' + currentX + 'px)'
-//   }, 16);
-// }
 
-function animate(endX, duration) {
+var curr_color, prev_color;
+color = curr_color = "grey";
+prev_color = "green";
+var tools = {}
+tools.paintBucket = false;
+tools.chooseColor = false;
+tools.move = false;
+tools.transform = false;
 
-  let currentX = node.offsetLeft;
-  const frameCount = duration / 1000 * 60;
-  const dx = (endX - currentX) / frameCount;
+let element;
 
-  const tick = () => {
-    currentX += dx;
-    console.log(currentX);
+// events that are triggered when you click on the block of canvas
+canvas.addEventListener('click', e => {
 
-    node.style.transform = 'translateX(' + currentX + 'px)'
+  console.log(e.clientX + ':' + e.clientY);
+  // choose figure on canvas
+  element = document.elementFromPoint(e.clientX, e.clientY);
 
-    if (currentX < endX) {
-      requestAnimationFrame(tick);
+  //paint over
+  if (tools.paintBucket) {
+    // if elem is on canvas
+    if (element.parentElement === canvas) {
+      element.style.backgroundColor = curr_color;
+      console.log(' ', curr_color, ' ', prev_color);
     }
+  }
 
-  };
+  //take the color of the figure 
+  if (tools.chooseColor) {
+    // if elem is on canvas
+    if (element.parentElement === canvas) {
+      refreshColor(element.style.backgroundColor);
+    }
+  }
 
-  tick(dx);
+  //!!!!!!!!
+  // move
+  if (tools.move) {
+    var rect = element.getBoundingClientRect()
+    var dx = e.pageX - rect.left,
+      dy = e.pageY - rect.top
+
+    // element.style.backgroundColor = "silver";
+    document.body.addEventListener("click", function handler(e) {
+
+      console.log("element.style", element.style.offsetLeft + ':' + element.style.offsetTop);
+      console.log("dx dy", dx + ':' + e.clientY);
+      console.log("e.client", e.clientX + ':' + dy);
+
+      element.style.offsetLeft = e.clientX - dx + 'px';
+      element.style.offsetTop = e.clientY - dy + 'px';
+      // document.removeEventListener('click', handler, true);
+      // event.stopPropagation();
+    });
+  }
+
+})
+
+// events that are triggered when you click on the block of choosing tools
+pallete.addEventListener('click', e => {
+
+  if (e.target === paintBucket) {
+    tools.paintBucket = true;
+    document.body.style.cursor = bucketCursor;
+  } else {
+    tools.paintBucket = false;
+  }
+
+  if (e.target === chooseColor) {
+    tools.chooseColor = true;
+    document.body.style.cursor = chooseColorCursor;
+  } else {
+    tools.chooseColor = false;
+  }
+
+  if (e.target === move) {
+    tools.move = true;
+    document.body.style.cursor = moveCursor;
+  } else {
+    tools.move = false;
+  }
+
+  if (e.target === transform) {
+    tools.transform = true;
+    document.body.style.cursor = transferCursor;
+  } else {
+    tools.transform = false;
+  }
+
+  if (!tools.paintBucket && !tools.chooseColor && !tools.move && !tools.transform) {
+    document.body.style.cursor = "";
+  }
+
+})
+
+// events that are triggered when you click on the block of choosing colors
+colors.addEventListener('click', e => {
+  if (tools.chooseColor) {
+    if (e.target === currColor) {
+      refreshColor(curr_color);
+    }
+    if (e.target === prevColor) {
+      refreshColor(prev_color);
+    }
+    if (e.target === redColor) {
+      refreshColor("red");
+    }
+    if (e.target == blueColor) {
+      refreshColor("blue");
+    }
+  }
+
+  //when you click do the standard cursor and the default action
+  resetAction();
+});
+
+
+// 
+var colorPicker = document.querySelector("#color");
+colorPicker.addEventListener("input", watchColorPicker, false);
+
+function watchColorPicker(event) {
+  refreshColor(event.target.value);
 }
 
-animate(300, 3000);
+function refreshColor(color) {
+  prev_color = curr_color;
+  curr_color = color;
+  currColor.childNodes[1].style.backgroundColor = curr_color;
+  prevColor.childNodes[1].style.backgroundColor = prev_color;
+}
+
+// for now just reset the color choices
+function resetAction() {
+  if (tools.chooseColor) {
+    tools.chooseColor = false;
+    document.body.style.cursor = "";
+  }
+}
